c84fe7b7e247ca2db3eb09e179bf863b8712b46a
Daniel Green <dan@shup.com>
2013-02-24 12:42:20 -0800

    adding some error checking to decay_value option

--- options.js
+++ options.js
@@ -3,7 +3,18 @@ function dt_save_options()
   var DT = chrome.extension.getBackgroundPage().DT;
   var interval_select = document.querySelector("#dt_decay_interval");
   var idx = interval_select.selectedIndex;
-  dt_set_setting(DT.storage_key("decay_value"), document.querySelector("#dt_decay_value").value);
+  var dv_elm = document.querySelector("#dt_decay_value");
+  var decay_value = parseInt(dv_elm.value);
+  
+  if (isNaN(decay_value))
+  {
+    dv_elm.style.border = "1px solid red";
+    document.getElementById("dt_save_status").innerHTML = "Invalid decay value";
+    return;
+  }
+  
+  dv_elm.style.border = "";
+  dt_set_setting(DT.storage_key("decay_value"), decay_value);
   dt_set_setting(DT.storage_key("decay_interval"), interval_select.options[idx].value);
   dt_set_setting(DT.storage_key("archive_enabled"), document.querySelector("#dt_archive_enabled").checked);
   dt_set_setting(DT.storage_key("enabled"), document.querySelector("#dt_enabled").checked);
@@ -20,7 +31,19 @@ function dt_save_options()
 function dt_options_init()
 {
   document.querySelector('#dt_save_settings').addEventListener('click', dt_save_options);
+  document.querySelector('#dt_reset_settings').addEventListener('click', dt_reset_options);
   dt_restore_options();
+}
+
+function dt_reset_options()
+{
+  dt_restore_options();
+  
+  var status = document.getElementById("dt_save_status");
+  status.innerHTML = "Options reset.";
+  setTimeout(function() {
+    status.innerHTML = "";
+  }, 750);
 }
 
 function dt_restore_options()
